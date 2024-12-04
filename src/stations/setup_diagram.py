8630bf2a98b54f6d9b1c0a82aa414493f85f4f93
Eric Chen <deeplearningericchen@gmail.com>
2024-12-04 16:18:13 -0500

    debug for merge points

--- src/stations/setup_diagram.py
+++ src/stations/setup_diagram.py
@@ -8,30 +8,37 @@ from pydrake.all import (
     MeshcatVisualizer,
     RigidTransform,
     RotationMatrix,
-    PortSwitch
+    PortSwitch,
 )
 from omegaconf import DictConfig
-from src.stations.teleop_station import MakePandaManipulationStation, get_directives, CreateArmOnlyPlant
+from src.stations.teleop_station import (
+    MakePandaManipulationStation,
+    get_directives,
+    CreateArmOnlyPlant,
+)
 from src.modules.perception import MergePointClouds, LNDFGrasper
 import logging
 import os
 import random
 import numpy as np
-from manipulation.station import (
-    DepthImageToPointCloud
-)
+from manipulation.station import DepthImageToPointCloud
 import hydra
 import plotly.express as px
 from src.modules.kinematics import Planner, AddPandaDifferentialIK
 import torch
 
+
 class NoDiffIKWarnings(logging.Filter):
     def filter(self, record):
         return not record.getMessage().startswith("Differential IK")
 
-def BuildPouringDiagram(meshcat: Meshcat, cfg: DictConfig) -> tuple[Diagram, Diagram, ]:
-    directives_config = cfg['directives']
-    lndf_config = cfg['lndf']
+
+def BuildPouringDiagram(meshcat: Meshcat, cfg: DictConfig) -> tuple[
+    Diagram,
+    Diagram,
+]:
+    directives_config = cfg["directives"]
+    lndf_config = cfg["lndf"]
 
     builder = DiagramBuilder()
 
@@ -47,21 +54,17 @@ def BuildPouringDiagram(meshcat: Meshcat, cfg: DictConfig) -> tuple[Diagram, Dia
 
     # get cameras from station
     num_cameras = 3
-    camera_body_indices=[
+    camera_body_indices = [
         plant.GetBodyIndices(plant.GetModelInstanceByName(f"camera{i}"))[0]
         for i in range(num_cameras)
     ]
-    cameras = [
-        station.GetSubsystemByName(f"camera{i}")
-        for i in range(3)
-    ]
+    cameras = [station.GetSubsystemByName(f"camera{i}") for i in range(3)]
 
     merge_point_clouds = builder.AddNamedSystem(
         "merge_point_clouds",
         MergePointClouds(
             plant,
             plant.GetModelInstanceByName("basket"),
-
             # only above view cameras to get partial point clouds
             camera_body_indices=[
                 plant.GetBodyIndices(plant.GetModelInstanceByName("camera0"))[0],
@@ -71,6 +74,7 @@ def BuildPouringDiagram(meshcat: Meshcat, cfg: DictConfig) -> tuple[Diagram, Dia
             ],
             cameras=cameras,
             meshcat=meshcat,
+            debug=True,
         ),
     )
 
@@ -100,7 +104,6 @@ def BuildPouringDiagram(meshcat: Meshcat, cfg: DictConfig) -> tuple[Diagram, Dia
             merge_point_clouds.GetInputPort(depth_port),
         )
 
-
     builder.Connect(
         panda_station.GetOutputPort("body_poses"),
         merge_point_clouds.GetInputPort("body_poses"),
@@ -110,22 +113,20 @@ def BuildPouringDiagram(meshcat: Meshcat, cfg: DictConfig) -> tuple[Diagram, Dia
     planner_system = Planner(meshcat, plant)
     planner = builder.AddSystem(planner_system)
     builder.Connect(
-        panda_station.GetOutputPort("body_poses"),
-        planner.GetInputPort("body_poses")
+        panda_station.GetOutputPort("body_poses"), planner.GetInputPort("body_poses")
     )
     builder.Connect(
-        grasper.GetOutputPort("grasp_pose"),
-        planner.GetInputPort("basket_grasp")
+        grasper.GetOutputPort("grasp_pose"), planner.GetInputPort("basket_grasp")
     )
 
-    #TODO: Fix Port names
+    # TODO: Fix Port names
     builder.Connect(
         station.GetOutputPort("panda_hand_state_estimated"),
-        planner.GetInputPort("hand_state")
+        planner.GetInputPort("hand_state"),
     )
     builder.Connect(
         station.GetOutputPort("panda_arm_position_measured"),
-        planner.GetInputPort("panda_position")
+        planner.GetInputPort("panda_position"),
     )
 
     # DiffIK
@@ -133,33 +134,34 @@ def BuildPouringDiagram(meshcat: Meshcat, cfg: DictConfig) -> tuple[Diagram, Dia
     arm_only_plant = CreateArmOnlyPlant(time_step)
 
     diff_ik = AddPandaDifferentialIK(builder, arm_only_plant)
-    builder.Connect(
-        planner.GetOutputPort("X_WG"),
-        diff_ik.GetInputPort("X_AE_desired")
-    )
+    builder.Connect(planner.GetOutputPort("X_WG"), diff_ik.GetInputPort("X_AE_desired"))
     builder.Connect(
         station.GetOutputPort("panda_arm_state_estimated"),
-        diff_ik.GetInputPort("robot_state")
+        diff_ik.GetInputPort("robot_state"),
     )
-    builder.Connect(planner.GetOutputPort("reset_diff_ik"),
-                    diff_ik.GetInputPort("use_robot_state"))
-    builder.Connect(planner.GetOutputPort("hand_position"),
-                    station.GetInputPort("panda_hand_position"))
-    
+    builder.Connect(
+        planner.GetOutputPort("reset_diff_ik"), diff_ik.GetInputPort("use_robot_state")
+    )
+    builder.Connect(
+        planner.GetOutputPort("hand_position"),
+        station.GetInputPort("panda_hand_position"),
+    )
+
     # switch between direct control and diff ik
     switch = builder.AddSystem(PortSwitch(7))
+    builder.Connect(diff_ik.get_output_port(), switch.DeclareInputPort("diff_ik"))
     builder.Connect(
-        diff_ik.get_output_port(),
-        switch.DeclareInputPort("diff_ik")
+        planner.GetOutputPort("panda_position_command"),
+        switch.DeclareInputPort("position"),
     )
-    builder.Connect(planner.GetOutputPort("panda_position_command"),
-                    switch.DeclareInputPort("position"))
-    builder.Connect(switch.get_output_port(),
-                    station.GetInputPort("panda_arm_position"))
-    builder.Connect(planner.GetOutputPort("control_mode"),
-                    switch.get_port_selector_input_port())
+    builder.Connect(
+        switch.get_output_port(), station.GetInputPort("panda_arm_position")
+    )
+    builder.Connect(
+        planner.GetOutputPort("control_mode"), switch.get_port_selector_input_port()
+    )
 
-    #Debug: visualize camera images
+    # Debug: visualize camera images
     # visualize_camera_images(station)
 
     # Debug: visualize depth images
